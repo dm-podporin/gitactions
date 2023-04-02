aflp91/microblog

Chapter 22: Background Jobs (v0.22)

config.py
@@ -20,4 +20,5 @@ class Config(object):
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URI = os.environ.get('ELASTICSEARCH_URI')
+    REDIS_URI = os.environ.get('REDIS_URI') or 'redis://'
     POSTS_PER_PAGE = 25

dm-podporin/gitactions

config.py
@@ -21,4 +21,5 @@ class Config(object):
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
     POSTS_PER_PAGE = 25